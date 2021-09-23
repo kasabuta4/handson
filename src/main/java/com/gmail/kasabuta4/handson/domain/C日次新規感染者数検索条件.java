33fb50ebab95d0a30aac8a4ac3955749f82a5d19
Akira Kawauchi <kasabuta4@gmail.com>
2021-09-23 15:22:20 +0900

    検索条件に表示期間を追加

--- src/main/java/com/gmail/kasabuta4/handson/domain/C日次新規感染者数検索条件.java
+++ src/main/java/com/gmail/kasabuta4/handson/domain/C日次新規感染者数検索条件.java
@@ -1,6 +1,7 @@
 package com.gmail.kasabuta4.handson.domain;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.validation.constraints.NotEmpty;
 
 public class C日次新規感染者数検索条件 implements Serializable {
@@ -10,7 +11,21 @@ public class C日次新規感染者数検索条件 implements Serializable {
   @NotEmpty(message="必須項目です")
   private String p都道府県;
 
+  private LocalDate p表示期間From;
+
+  private LocalDate p表示期間To;
+
   public C日次新規感染者数検索条件() {
+  }
+
+  public C日次新規感染者数検索条件 normalized() {
+    C日次新規感染者数検索条件 result = new C日次新規感染者数検索条件();
+
+    result.p都道府県 = p都道府県;
+    result.p表示期間From = p表示期間From == null ? LocalDate.of(2019, 1, 1) : p表示期間From;
+    result.p表示期間To = p表示期間To == null ? LocalDate.now() : p表示期間To;
+
+    return result;
   }
 
   public String getP都道府県() {
@@ -19,5 +34,21 @@ public class C日次新規感染者数検索条件 implements Serializable {
 
   public void setP都道府県(String p都道府県) {
     this.p都道府県 = p都道府県;
+  }
+
+  public LocalDate getP表示期間From() {
+    return p表示期間From;
+  }
+
+  public void setP表示期間From(LocalDate p表示期間From) {
+    this.p表示期間From = p表示期間From;
+  }
+
+  public LocalDate getP表示期間To() {
+    return p表示期間To;
+  }
+
+  public void setP表示期間To(LocalDate p表示期間To) {
+    this.p表示期間To = p表示期間To;
   }
 }
--- src/main/java/com/gmail/kasabuta4/handson/infrastructure/jpa/Jpa日次新規感染者数Dao.java
+++ src/main/java/com/gmail/kasabuta4/handson/infrastructure/jpa/Jpa日次新規感染者数Dao.java
@@ -4,6 +4,7 @@ import com.gmail.kasabuta4.handson.domain.C日次新規感染者数;
 import com.gmail.kasabuta4.handson.domain.C日次新規感染者数Dao;
 import com.gmail.kasabuta4.handson.domain.C日次新規感染者数検索条件;
 import java.sql.Date;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 import java.util.List;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
@@ -21,14 +22,22 @@ public class Jpa日次新規感染者数Dao implements C日次新規感染者数
       + "FROM "
       + "    NEWLY_CONFIRMED_CASES_DAILY "
       + "WHERE "
-      + "    PREFECTURE = ?";
+      + "        PREFECTURE = ? "
+      + "    AND REPORTED_DATE >= ? "
+      + "    AND REPORTED_DATE <= ? "
+      + "ORDER BY "
+      + "    REPORTED_DATE";
 
   @Inject @JsfDemoDB private EntityManager em;
 
   @Override
   public List<C日次新規感染者数> search(C日次新規感染者数検索条件 検索条件) {
+    C日次新規感染者数検索条件 normalized検索条件 = 検索条件.normalized();
+
     Stream rawStream = em.createNativeQuery(QUERY_STRING)
-        .setParameter(1, 検索条件.getP都道府県())
+        .setParameter(1, normalized検索条件.getP都道府県())
+        .setParameter(2, normalized検索条件.getP表示期間From().format(ISO_LOCAL_DATE))
+        .setParameter(3, normalized検索条件.getP表示期間To().format(ISO_LOCAL_DATE))
         .getResultStream();
 
     @SuppressWarnings("unchecked")
