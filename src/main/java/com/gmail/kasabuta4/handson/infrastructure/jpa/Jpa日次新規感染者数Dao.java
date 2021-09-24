30701b3f2a12023e71b2f1001aac501017286434
Akira Kawauchi <kasabuta4@gmail.com>
2021-09-24 22:10:54 +0900

    SQL文をプロパティファイルに移動

--- src/main/java/com/gmail/kasabuta4/handson/infrastructure/jpa/Jpa日次新規感染者数Dao.java
+++ src/main/java/com/gmail/kasabuta4/handson/infrastructure/jpa/Jpa日次新規感染者数Dao.java
@@ -3,9 +3,11 @@ package com.gmail.kasabuta4.handson.infrastructure.jpa;
 import com.gmail.kasabuta4.handson.domain.C日次新規感染者数;
 import com.gmail.kasabuta4.handson.domain.C日次新規感染者数Dao;
 import com.gmail.kasabuta4.handson.domain.C日次新規感染者数検索条件;
+import java.io.IOException;
 import java.sql.Date;
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 import java.util.List;
+import java.util.Properties;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
 import javax.enterprise.context.ApplicationScoped;
@@ -15,26 +17,28 @@ import javax.persistence.EntityManager;
 @ApplicationScoped
 public class Jpa日次新規感染者数Dao implements C日次新規感染者数Dao {
 
-  private static final String QUERY_STRING
-      = "SELECT "
-      + "    REPORTED_DATE, "
-      + "    CASES "
-      + "FROM "
-      + "    NEWLY_CONFIRMED_CASES_DAILY "
-      + "WHERE "
-      + "        PREFECTURE = ? "
-      + "    AND REPORTED_DATE >= ? "
-      + "    AND REPORTED_DATE <= ? "
-      + "ORDER BY "
-      + "    REPORTED_DATE";
+  private static final String SQL_STATEMENTS_FILE = "Jpa日次新規感染者数DaoSql.properties";
+  
+  private static final Properties SQL_STATEMENTS;
+
+  private static final String SEARCH_SQL_KEY = "Jpa日次新規感染者数DaoSql.SEARCH";
 
   @Inject @JsfDemoDB private EntityManager em;
+
+  static {
+    SQL_STATEMENTS = new Properties();
+    try {
+      SQL_STATEMENTS.load(Jpa日次新規感染者数Dao.class.getResourceAsStream(SQL_STATEMENTS_FILE));
+    } catch (IOException e) {
+      // ignore
+    }
+  }
 
   @Override
   public List<C日次新規感染者数> search(C日次新規感染者数検索条件 検索条件) {
     C日次新規感染者数検索条件 normalized検索条件 = 検索条件.normalized();
 
-    Stream rawStream = em.createNativeQuery(QUERY_STRING)
+    Stream rawStream = em.createNativeQuery(SQL_STATEMENTS.getProperty(SEARCH_SQL_KEY))
         .setParameter(1, normalized検索条件.getP都道府県())
         .setParameter(2, normalized検索条件.getP表示期間From().format(ISO_LOCAL_DATE))
         .setParameter(3, normalized検索条件.getP表示期間To().format(ISO_LOCAL_DATE))
